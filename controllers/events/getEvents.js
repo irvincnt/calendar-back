5592d56560275b20f2e7fef852753c40f24eaf9e
irvincnt <irvincnt93@gmail.com>
2021-02-24 11:39:57 -0600

    get all events

--- controllers/events/getEvents.js
+++ controllers/events/getEvents.js
@@ -1,10 +1,16 @@
 const { response } = require('express');
+const Event = require('../../models/Event');
 
-const getEvents = (req, res = response) => {
-  res.json({
-    ok: true,
-    msg: 'Obtener evento'
-  })
+const getEvents = async(req, res = response) => {
+
+const events = await Event.find()
+                          .populate('user', 'name')
+
+res.json({
+  ok: true,
+  events
+})
+
 }
 
 module.exports = {
