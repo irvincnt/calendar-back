347073e6b2f5adf65cbb9b2d485cd4d82d28efef
irvincnt <irvincnt93@gmail.com>
2021-02-24 11:58:26 -0600

    Edit event

--- controllers/events/updateEvent.js
+++ controllers/events/updateEvent.js
@@ -1,10 +1,49 @@
 const { response } = require('express');
+const Event = require('../../models/Event');
 
-const updateEvent = (req, res = response) => {
-  res.json({
-    ok: true,
-    msg: 'Actualizar evento'
-  })
+const updateEvent = async(req, res = response) => {
+  const eventId = req.params.id
+  const uid = req.uid
+
+  try {
+    const event = await Event.findById(eventId);
+
+    if (!event) {
+      res.status(404).json({
+        ok: false,
+        msg: 'El evento no existe'
+      })
+    }
+
+    if (event.user.toString() !== uid) {
+      res.status(401).json({
+        ok: false,
+        msg: 'No tienes permisos para editar el evento'
+      })
+    }
+
+    const newEvent = {
+      ...req.body,
+      user: uid
+    }
+
+    const updatedEvent = await Event.findByIdAndUpdate(
+      eventId,
+      newEvent, 
+      { new: true} 
+    )
+
+    res.status(200).json({
+      ok: true,
+      event: updatedEvent
+    })
+    
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      msg: 'Error - Comunicate con el administrador'
+    })
+  }
 }
 
 module.exports = {
